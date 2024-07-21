b5996db30f303f700bf874d89051dd1fd4c866b1
sarmhi <sammiejay813@gmail.com>
2024-07-21 23:06:06 +0100

    chore: form submission logic completed

--- src/components/Contactform.tsx
+++ src/components/Contactform.tsx
@@ -74,147 +74,145 @@ export default function Contactform() {
   };
 
   return (
-    <>
-      {loading ? (
-        <div className="flex justify-center pt-[60px]">
-          <FadeLoader
-            color={'#e64d15'}
-            loading={loading}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </div>
-      ) : (
-        <Formik
-          initialValues={initialState}
-          validate={validate}
-          onSubmit={async (_, { setSubmitting, resetForm }) => {
-            setSubmitting(true);
-            setLoading(true);
+    <Formik
+      initialValues={initialState}
+      validate={validate}
+      onSubmit={async (_, { setSubmitting, resetForm }) => {
+        setSubmitting(true);
+        setLoading(true);
 
-            fetch(scriptUrl, {
-              method: 'POST',
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              body: new FormData(formRef.current!),
-            })
-              // eslint-disable-next-line @typescript-eslint/no-unused-vars
-              .then((_) => {
-                console.log('SUCCESSFULLY SUBMITTED');
-                setLoading(false);
-                setSubmitting(false);
-                resetForm({ values: initialState });
-              })
-              .catch(() => {
-                setLoading(false);
-                setSubmitting(false);
-              });
-          }}
+        fetch(scriptUrl, {
+          method: 'POST',
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          body: new FormData(formRef.current!),
+        })
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          .then((_) => {
+            console.log('SUCCESSFULLY SUBMITTED');
+            setLoading(false);
+            setSubmitting(false);
+            resetForm({ values: initialState });
+          })
+          .catch(() => {
+            setLoading(false);
+            setSubmitting(false);
+          });
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleSubmit,
+        handleBlur,
+        isSubmitting,
+        // setFieldValue,
+      }) => (
+        <div
+          id="contact"
+          className="md:items-center md:grid md:grid-cols-2 px-5 py-8 gap-10"
         >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleSubmit,
-            handleBlur,
-            isSubmitting,
-            // setFieldValue,
-          }) => (
-            <div
-              id="contact"
-              className="md:items-center md:grid md:grid-cols-2 px-5 py-8 gap-10"
-            >
-              <div>
-                <div className="flex flex-col gap-5 px-2 text-center">
-                  <h1 className="font-bold text-4xl md:text-6xl text-[#090916]">
-                    Get in Touch
-                  </h1>
-                  <p className="font-bold text-base text-[#646262]">
-                    We're here to answer your questions and discuss your needs.
-                  </p>
-                </div>
+          <div>
+            <div className="flex flex-col gap-5 px-2 text-center">
+              <h1 className="font-bold text-4xl md:text-6xl text-[#090916]">
+                Get in Touch
+              </h1>
+              <p className="font-bold text-base text-[#646262]">
+                We're here to answer your questions and discuss your needs.
+              </p>
+            </div>
 
-                <form
-                  className="gap-5 flex flex-col mt-6"
-                  onSubmit={handleSubmit}
-                  ref={formRef}
-                >
-                  <Input
-                    type="text"
-                    name="name"
-                    placeholder="Name *"
-                    value={values.name}
-                    onBlur={handleBlur}
-                    changed={handleChange}
-                    error={errors.name && touched.name && errors.name}
-                  />
-                  <Input
-                    type="email"
-                    name="email"
-                    placeholder="Email *"
-                    value={values.email}
-                    changed={handleChange}
-                    onBlur={handleBlur}
-                    error={errors.email && touched.email && errors.email}
-                  />
-                  <Input
-                    type="tel"
-                    name="phone"
-                    placeholder="Phone Number *"
-                    value={values.phone}
-                    changed={handleChange}
-                    onBlur={handleBlur}
-                    error={errors.phone && touched.phone && errors.phone}
-                  />
-                  <TextAreaInput
-                    classDef="w-[100%]"
-                    name="enquiries"
-                    placeholder="How can i partner with you?"
-                    value={values.enquiries}
-                    changed={handleChange}
-                    onBlur={handleBlur}
-                    error={
-                      errors.enquiries && touched.enquiries && errors.enquiries
-                    }
-                  />
-
-                  <Button
-                    title="Send"
-                    disabled={handleDisable(values, isSubmitting)}
-                    classDefinition="bg-black hover:bg-[#242442] text-white h-12 border px-5 py-3 gap-2 border-solid border-gray-500 font-bold text-base"
-                  />
-
-                  <div className="flex flex-col md:flex-row md:justify-between gap-3 mt-5">
-                    {formContact.map((contact) => (
-                      <div
-                        className="flex gap-2 items-center font-bold text-[#05050B]"
-                        key={contact.title}
-                      >
-                        <img
-                          src={contact.logo}
-                          alt="logo"
-                          className="w-7 h-7"
-                        />
-                        <div className="text-xs leading-tight">
-                          <p className="whitespace-nowrap">{contact.title}</p>
-                          <p className="break-words">{contact.value}</p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </form>
-              </div>
-              <div className="mt-8 md:mt-0">
-                <img
-                  src={ContactIMage}
-                  alt="contact"
-                  className="object-cover object-center w-full h-full"
+            {loading && (
+              <div className="flex justify-center pt-[60px]">
+                <FadeLoader
+                  color={'#242442'}
+                  loading={loading}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
                 />
               </div>
-            </div>
-          )}
-        </Formik>
+            )}
+
+            <form
+              className="gap-5 flex flex-col mt-6"
+              onSubmit={handleSubmit}
+              ref={formRef}
+            >
+              <Input
+                type="text"
+                name="name"
+                placeholder="Name *"
+                value={values.name}
+                onBlur={handleBlur}
+                changed={handleChange}
+                error={errors.name && touched.name && errors.name}
+              />
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email *"
+                value={values.email}
+                changed={handleChange}
+                onBlur={handleBlur}
+                error={errors.email && touched.email && errors.email}
+              />
+              <Input
+                type="tel"
+                name="phone"
+                placeholder="Phone Number *"
+                value={values.phone}
+                changed={handleChange}
+                onBlur={handleBlur}
+                error={errors.phone && touched.phone && errors.phone}
+              />
+              <TextAreaInput
+                classDef="w-[100%]"
+                name="enquiries"
+                placeholder="How can i partner with you?"
+                value={values.enquiries}
+                changed={handleChange}
+                onBlur={handleBlur}
+                error={
+                  errors.enquiries && touched.enquiries && errors.enquiries
+                }
+              />
+
+              <Button
+                title="Send"
+                disabled={handleDisable(values, isSubmitting)}
+                classDefinition={`${
+                  !handleDisable(values, isSubmitting)
+                    ? 'bg-black hover:bg-[#242442]'
+                    : 'bg-gray-600 cursor-not-allowed'
+                } text-white h-12 border px-5 py-3 gap-2 border-solid border-gray-500 font-bold text-base`}
+              />
+
+              <div className="flex flex-col md:flex-row md:justify-between gap-3 mt-5">
+                {formContact.map((contact) => (
+                  <div
+                    className="flex gap-2 items-center font-bold text-[#05050B]"
+                    key={contact.title}
+                  >
+                    <img src={contact.logo} alt="logo" className="w-7 h-7" />
+                    <div className="text-xs leading-tight">
+                      <p className="whitespace-nowrap">{contact.title}</p>
+                      <p className="break-words">{contact.value}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </form>
+          </div>
+          <div className="mt-8 md:mt-0">
+            <img
+              src={ContactIMage}
+              alt="contact"
+              className="object-cover object-center w-full h-full"
+            />
+          </div>
+        </div>
       )}
-    </>
+    </Formik>
   );
 }
